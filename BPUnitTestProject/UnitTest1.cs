e4aff9f26911402e5a7b7c2fcf9111b9bf8e3a69
Magesh Nandikkara <x00218707@mytudublin.ie>
2024-11-03 13:27:50 +0000

    More Unit Tests

--- BPUnitTestProject/UnitTest1.cs
+++ BPUnitTestProject/UnitTest1.cs
@@ -23,7 +23,14 @@ namespace BPUnitTestProject
         [TestMethod]
         public void UnitTestPreHighScenario1()
         {
-            BloodPressure BP = new() { Systolic = 130, Diastolic = 85 };
+            BloodPressure BP = new() { Systolic = 130, Diastolic = 75 };
+            Assert.AreEqual(BP.Category, BPCategory.PreHigh);
+        }
+
+        [TestMethod]
+        public void UnitTestPreHighScenario2()
+        {
+            BloodPressure BP = new() { Systolic = 110, Diastolic = 85 };
             Assert.AreEqual(BP.Category, BPCategory.PreHigh);
         }
 
@@ -32,6 +39,34 @@ namespace BPUnitTestProject
         {
             BloodPressure BP = new() { Systolic = 170, Diastolic = 95 };
             Assert.AreEqual(BP.Category, BPCategory.High);
+        }
+
+        [TestMethod]
+        public void SystolicValueTooLow()
+        {
+            BloodPressure BP = new() { Systolic = 60, Diastolic = 50 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BP.Category);
+        }
+
+        [TestMethod]
+        public void SystolicValueTooHigh()
+        {
+            BloodPressure BP = new() { Systolic = 200, Diastolic = 50 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BP.Category);
+        }
+
+        [TestMethod]
+        public void DiastolicValueTooLow()
+        {
+            BloodPressure BP = new() { Systolic = 100, Diastolic = 30 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BP.Category);
+        }
+
+        [TestMethod]
+        public void DiastolicValueTooHigh()
+        {
+            BloodPressure BP = new() { Systolic = 100, Diastolic = 110 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BP.Category);
         }
 
     }
