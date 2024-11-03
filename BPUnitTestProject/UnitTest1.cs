f4bd4ab459a7453c5de06225935e659eeae0e4ed
Magesh Nandikkara <X00218707@mytudublin.ie>
2024-11-03 02:06:39 +0000

    Renamed Unit Test Methods

--- BPUnitTestProject/UnitTest1.cs
+++ BPUnitTestProject/UnitTest1.cs
@@ -7,28 +7,28 @@ namespace BPUnitTestProject
     public class UnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UnitTestLow()
         {
             BloodPressure BP = new() { Systolic = 80, Diastolic = 50 };
             Assert.AreEqual(BP.Category, BPCategory.Low);
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void UnitTestIdeal()
         {
             BloodPressure BP = new() { Systolic = 110, Diastolic = 70 };
             Assert.AreEqual(BP.Category, BPCategory.Ideal);
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void UnitTestPreHighScenario1()
         {
             BloodPressure BP = new() { Systolic = 130, Diastolic = 85 };
             Assert.AreEqual(BP.Category, BPCategory.PreHigh);
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TeUnitTestHigh()
         {
             BloodPressure BP = new() { Systolic = 170, Diastolic = 95 };
             Assert.AreEqual(BP.Category, BPCategory.High);
